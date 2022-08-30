4c7bf8799b6fb91cd16d543b17f73ac966351b5a
yumin-jung <jungym887@naver.com>
2022-08-30 15:04:11 +0900

    Solved 15552 Go

--- baekjoon/15552.go
+++ baekjoon/15552.go
@@ -1,1 +1,23 @@
-bufio
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var x, a, b int
+
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+
+	defer writer.Flush()
+
+	fmt.Scanf("%d", &x)
+
+	for i := 0; i < x; i++ {
+		fmt.Fscan(reader, &a, &b)
+		fmt.Fprintln(writer, a+b)
+	}
+}
