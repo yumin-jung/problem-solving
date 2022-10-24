0db3ace6d6dc457c45e9364c628b37160853822e
yumin-jung <jungym887@naver.com>
2022-10-24 22:22:03 +0900

    Solved 9498 Python

---
+++ baekjoon/9498.py
@@ -0,0 +1,15 @@
+a = input()
+a = int(a)
+
+if a > 100:
+    print("F")
+elif a >= 90:
+    print("A")
+elif a >= 80:
+    print("B")
+elif a >= 70:
+    print("C")
+elif a >= 60:
+    print("D")
+else:
+    print("F")
