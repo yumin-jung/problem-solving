a742c3f1616cd78f52b66b5770c9ce46abdf3850
yumin-jung <jungym887@naver.com>
2023-01-26 00:47:12 +0900

    Update problems solved by 2023-01-26

---
+++ programmers/Lv.2/피보나치-수.js
@@ -0,0 +1,7 @@
+function solution(n) {
+    let fibonacci = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        fibonacci.push((fibonacci[i - 1] + fibonacci[i - 2]) % 1234567);
+    }
+    return fibonacci[fibonacci.length - 1];
+}
