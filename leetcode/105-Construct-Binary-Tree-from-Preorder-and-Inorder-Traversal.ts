2dda48e86070634e7f4fe0ca47e67eca5dc6e51a
yumin-jung <jungym887@naver.com>
2023-02-16 18:44:15 +0900

    Runtime: 75 ms (69.80%), Memory: 46.2 MB (77.96%)

---
+++ leetcode/105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.ts
@@ -0,0 +1,17 @@
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+}
+
+const maxDepth = (root: TreeNode | null): number => {
+    if (root === null) return 0;
+    let left = maxDepth(root.left);
+    let right = maxDepth(root.right);
+    return 1 + Math.max(left, right);
+};
