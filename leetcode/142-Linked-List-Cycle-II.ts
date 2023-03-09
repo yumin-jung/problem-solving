de780aed1f4c923772309eefea77eeaec85ace73
yumin-jung <jungym887@naver.com>
2023-03-09 18:30:07 +0900

    Runtime: 76 ms (90.41%), Memory: 46.2 MB (27.29%)

---
+++ leetcode/142-Linked-List-Cycle-II.ts
@@ -0,0 +1,19 @@
+const detectCycle = (head: ListNode | null): ListNode | null => {
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) {
+            slow = head;
+            while (slow !== fast) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return slow;
+        }
+    }
+
+    return null;
+};
