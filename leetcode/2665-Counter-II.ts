280884acf0048dc535301d35b6cea71bd775e803
yumin-jung <jungym887@naver.com>
2023-05-07 15:46:06 +0900

    Runtime: 79 ms (29.82%), Memory: 44.9 MB (98.25%)

---
+++ leetcode/2665-Counter-II.ts
@@ -0,0 +1,21 @@
+type ReturnObj = {
+    increment: () => number,
+    decrement: () => number,
+    reset: () => number,
+}
+
+function createCounter(init: number): ReturnObj {
+    let presentCount = init;
+    return {
+        increment: () => ++presentCount,
+        decrement: () => --presentCount,
+        reset: () => presentCount = init,
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
