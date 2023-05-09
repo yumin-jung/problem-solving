53074b6da22ff5a9062ff9c917c38d8f2f8b92f1
yumin-jung <jungym887@naver.com>
2023-05-09 13:33:01 +0900

    Runtime: 70 ms (13.65%), Memory: 43.4 MB (50.72%)

---
+++ leetcode/2634-Filter-Elements-from-Array.ts
@@ -0,0 +1,11 @@
+function filter(arr: number[], fn: (n: number, i: number) => any): number[] {
+    let res = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            res.push(arr[i]);
+        }
+    }
+
+    return res;
+};
