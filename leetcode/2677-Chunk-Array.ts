395898be63cac29daebd2dd9a42616ebfd78ede5
yumin-jung <jungym887@naver.com>
2023-05-25 13:33:36 +0900

    Runtime: 70 ms (50.72%), Memory: 45.7 MB (57.55%)

---
+++ leetcode/2677-Chunk-Array.ts
@@ -0,0 +1,9 @@
+function chunk(arr: any[], size: number): any[][] {
+    const res = [];
+
+    for (let i = 0; i < arr.length; i += size) {
+        res.push(arr.slice(i, i + size));
+    }
+
+    return res;
+};
