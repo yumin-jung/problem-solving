b8a0ef92ffbfa94e9ec8331d1d3a9ece46e8785d
yumin-jung <jungym887@naver.com>
2023-05-28 10:01:57 +0900

    Runtime: 143 ms (77.14%), Memory: 78.2 MB (5.55%)

---
+++ leetcode/2631-Group-By.ts
@@ -0,0 +1,21 @@
+declare global {
+    interface Array<T> {
+        groupBy(fn: (item: T) => string): Record<string, T[]>
+    }
+}
+
+Array.prototype.groupBy = function (fn) {
+    const res = {};
+
+    for (let item of this) {
+        const key = fn(item);
+        res[key] ||= [];
+        res[key].push(item);
+    }
+
+    return res;
+}
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
