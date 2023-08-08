a43d8749689bd35c566b089c1e97044cf98f650d
yumin-jung <jungym887@naver.com>
2023-08-08 12:42:29 +0900

    Runtime: 58 ms (79.86%), Memory: 44.2 MB (62.1%)

---
+++ leetcode/33-Search-in-Rotated-Sorted-Array.ts
@@ -0,0 +1,26 @@
+function search(nums: number[], target: number): number {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) return mid;
+
+        if (nums[left] <= nums[mid]) {
+            if (target >= nums[left] && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            if (target > nums[mid] && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+};
